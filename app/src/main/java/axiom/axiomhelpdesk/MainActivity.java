15878518c29e92f32422ac1e1c04b1415b869c7f
unknown <b_ashish@ATAEHW0244.Axiom.Local>
2015-12-17 12:50:30 +0400

    First commit

---
+++ app/src/main/java/axiom/axiomhelpdesk/MainActivity.java
@@ -0,0 +1,19 @@
+package axiom.axiomhelpdesk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.axiomhelpdesk.com");
+        webView.setWebViewClient(new WebViewController());
+
+    }
+}
